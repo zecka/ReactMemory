946f421f6ca12f737f9cf0136b71565fce469d03
ZecKa <ferrari.robin@gmail.com>
2018-06-18 18:28:25 +0200

    Réagissez aux événements

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import GuessCount from './GuessCount';
 import Card from './card';
 
 class App extends Component {
+  handleCardClick(card){
+    console.log(card, 'clicked')
+  }
   render() {
     return (
       <div className="memory">
         <GuessCount guesses={0} />
-        <Card card="😭" feedback="hidden" />
-        <Card card="😁" feedback="justMatched" />
-        <Card card="🤣" feedback="justMismatched" />
-        <Card card="🎉" feedback="visible" />
-        <Card card="🎆" feedback="hidden" />
-        <Card card="👌" feedback="justMatched" />
+        <Card card="🐕" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🎩" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card card="🤣" feedback="justMismatched" onClick={this.handleCardClick} />
+        <Card card="🎉" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="🎆" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="👌" feedback="justMatched" onClick={this.handleCardClick} />
       </div>
     )
   }
--- src/card.js
+++ src/card.js
@@ -3,8 +3,8 @@ import './card.css'
 
 const HIDDEN_SYMBOL = '?'
 
-const Card = ({ card, feedback }) => (
-    <div className={`card ${feedback}`}>
+const Card = ({ card, feedback, onClick }) => (
+    <div className={`card ${feedback}`} onClick={() => onClick(card)}>
         <span className="symbol">
             {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
         </span>

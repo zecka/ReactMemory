ce13d2f5489553ce1a1573a577c29454bc397670
ZecKa <ferrari.robin@gmail.com>
2018-06-18 17:27:21 +0200

    Start clean with debut-jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function CoolComponent({adject}){
-  return <p> Youpi so cool {adject} !! </p>
-}
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <CoolComponent adject="yea" />
-        <CoolComponent adject="powaa" />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div className="memory" />
   }
 }
 
-export default App;
+export default App
